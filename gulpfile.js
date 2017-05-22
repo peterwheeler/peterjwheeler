72868d325a13d3ce415fd0b9a5318f5e7a3a8431
peterwheeler <P.Wheeler@soton.ac.uk>
2017-05-22 10:21:01 +0100

    Created docs folder and build process

--- gulpfile.js
+++ gulpfile.js
@@ -13,14 +13,15 @@ var useref = require('gulp-useref');
 var gulpIf = require('gulp-if');
 var uglify = require('gulp-uglify');
 var cssnano = require('gulp-cssnano');
+var imagemin = require('gulp-imagemin');
 var rename = require('gulp-rename');
 var historyApiFallback = require('connect-history-api-fallback');
 var webpackDevMiddleware = require('webpack-dev-middleware');
 var webpackHotMiddleware = require('webpack-hot-middleware');
+var WriteFilePlugin = require('write-file-webpack-plugin');
 var runSequence = require('run-sequence');
 var path = require('path');
 var bundler;
-
 
 // --------------------------------- //
 // Set variables for gulp to run in  //
@@ -28,12 +29,7 @@ var bundler;
 
 var env = process.env.NODE_ENV;
 var babelenv = process.env.BABEL_ENV;
-var source, directory, images;
-
-gulp.task('set-variables', function() {
-    source = 'build';
-    directory = env === 'development' ? "build" : env === 'production' ? "dist" : env === 'docs' ? "docs" : "build";
-});
+var source, directory, webpackSettings;
 
 // ------------------------------ //
 // Error handler for running gulp //
@@ -52,26 +48,24 @@ var onError = function(err) {
 // -------------- //
 // Config comes from https://words.mxbry.com/making-react-webpack-browsersync-gulp-play-nice-and-hot-reload-b2c1e01522e3 //
 
-var webpackSettings = {
+var webpackDevelopment = {
   entry: {
     app: [
         "webpack/hot/dev-server?reload",
         "webpack-hot-middleware/client",
         "react-hot-loader/patch",
-        path.join(process.cwd(), "build/js/app/Main.jsx")
+        path.join(__dirname, "build/js/app/Main.jsx")
     ]
   },
   output: {
-    path: path.join(process.cwd(), "build/js/"),
+    path: path.join(__dirname, "build/js/"),
     filename: "bundle.js",
     publicPath: "/js/"
   },
-  plugins: environments.production() ? [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.UglifyJsPlugin()
-  ] : [
+  plugins: [
     new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    new WriteFilePlugin()
   ],
   resolve: {
     extensions: ['.js', '.jsx']
@@ -81,7 +75,7 @@ var webpackSettings = {
   module: {
     loaders: [{
         test: /\.(js|jsx)$/,
-        include: path.join(process.cwd(), "build/js/"),
+        include: path.join(__dirname, "build/js/"),
         exclude: /(node_modules|bower_components)/,
         loader: ['babel-loader?presets[]=react,presets[]=es2015', 'webpack-module-hot-accept']
       },
@@ -105,6 +99,55 @@ var webpackSettings = {
   }
 }
 
+var webpackProduction = {
+  entry: path.join(__dirname, "build/js/app/Main.jsx"),
+  output: {
+    path: path.join(__dirname, "docs/js"),
+    filename: "bundle.js",
+    publicPath: "/js/"
+  },
+  plugins: [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin(),
+    new WriteFilePlugin()
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devtool: 'cheap-module-source-map',
+  module: {
+      loaders: [{
+        test: /\.(js|jsx)$/,
+        include: path.join(__dirname, "build/js/"),
+        exclude: /(node_modules|bower_components)/,
+        loader: ['babel-loader?presets[]=react,presets[]=es2015']
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        loader: 'url-loader',
+        options: {
+          name: '../images/img-[hash:6].[ext]',
+          limit: 350000
+        }
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[hash:6].[ext]',
+          limit: 350000
+        }
+      }
+    ]
+  }
+}
+
+gulp.task('set-variables', function() {
+    source = 'build';
+    directory = env === 'development' ? "build" : env === 'production' ? "docs" : "development";
+    webpackSettings = env === 'development' ? webpackDevelopment : env === 'production' ? webpackProduction : webpackDevelopment;
+});
+
 gulp.task('webpack', function(callback) {
   bundler = webpack(webpackSettings);
 });
@@ -125,22 +168,17 @@ gulp.task('sass-task', function() {
 // Serve and watch build //
 // --------------------- //
 
-gulp.task('dev', function(){
+gulp.task('serve', function(){
   browserSync.init({
     server: {
       baseDir: ["./", "./" + directory + "/"]
     },
     port: 4000,
     browser: "C://Users//pw8g08//AppData//Local//Google//Chrome SxS//Application//chrome.exe",
-    middleware: [
-      webpackDevMiddleware(bundler, {
-          publicPath: webpackSettings.output.publicPath,
-          stats: { colors: true }
-        }),
-      webpackHotMiddleware(bundler)
-    ],
+    middleware: env === 'development' ? [webpackDevMiddleware(bundler, {publicPath: webpackDevelopment.output.publicPath, stats: { colors: true } }), webpackHotMiddleware(bundler)] :
+                        env === 'production' ? historyApiFallback() : [webpackDevMiddleware(bundler, {publicPath: webpackDevelopment.output.publicPath, stats: { colors: true } }), webpackHotMiddleware(bundler)],
     files: [
-      path.join(process.cwd(), directory + '/**/*.html')
+      path.join(__dirname, directory + '/**/*.html')
     ]
   })
 })
@@ -153,9 +191,11 @@ gulp.task('useref', function(){
   return gulp.src(source + "/index.html")
     .pipe(useref())
     // Minifies only if it's a JavaScript file
-    .pipe(gulpIf("*.js", uglify()))
+    // .pipe(gulpIf("*.js", uglify()))
+     // .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()); })
     // Minifies only if it's a CSS file
     .pipe(gulpIf("*.css", cssnano()))
+      .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()); })
     .pipe(gulp.dest(directory));
 });
 
@@ -166,13 +206,14 @@ gulp.task('css', function() {
 });
 
 gulp.task('images', function(){
-	return gulp.src(source + "/images/**/*.+(png|jpg|gif|svg)")
+  return gulp.src(source + "/images/**/*")
+	// return gulp.src(source + "/images/**/*.+(png|jpg|gif|svg)")
 		.pipe(imagemin())
 		.pipe(gulp.dest(directory + "/images"));
 });
 
 gulp.task('watch', function() {
-    gulp.watch(path.join(process.cwd(), source + "/sass/") + "**/*.scss", ['sass-task']);
+    gulp.watch(path.join(__dirname, source + "/sass/") + "**/*.scss", ['sass-task']);
 });
 
 // -------------------------- //
@@ -197,14 +238,14 @@ gulp.task('env-docs', function() {
     return env = 'docs';
 });
 
-gulp.task('default', ['env-dev', 'set-variables'], function(){
-  runSequence('sass-task', ['webpack', 'dev'], 'watch')
+gulp.task('default', ['env-dev'], function(){
+  runSequence('set-variables', 'sass-task', ['webpack', 'serve'], 'watch')
 });
 
-gulp.task('build', ['env-prod', 'set-variables'], function(){
-  runSequence('sass-task', ['webpack', 'dev'], 'watch')
+gulp.task('build', ['env-prod'], function(){
+  runSequence('set-variables', 'sass-task', ['webpack', 'serve'], 'watch')
 });
 
-gulp.task('docs', ['env-docs', 'set-variables'], function(){
-  runSequence('sass-task', ['webpack', 'dev'], 'watch')
+gulp.task('docs', ['env-docs'], function(){
+  runSequence('set-variables', 'sass-task', 'useref', ['webpack', 'serve'], 'watch')
 });
